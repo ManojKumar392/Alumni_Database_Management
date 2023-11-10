59509e1dd9b068c93d22685dcf310e3d78d3521e
Manoj Kumar R <manoj2003r@gmail.com>
2023-11-10 13:51:22 +0530

    Update settings.py
    removed all private information

--- alumni_bckend/alumni_backend/settings.py
+++ alumni_bckend/alumni_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x+i4=$tbq+1dj5dniijzdw0ho(xv%z)6v%@^e(xkm9upp&6cs8'
+SECRET_KEY = #private
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,7 +87,7 @@ DATABASES = {
 		'ENGINE': 'django.db.backends.mysql',
 		'NAME': 'alumni_database',
 		'USER': 'alumni',
-		'PASSWORD': 'DarkandSteel625',
+		'PASSWORD': ,#your password
 		'HOST':'localhost',
 		'PORT':'3306',
 	}
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'manoj2003r@gmail.com'
-EMAIL_HOST_PASSWORD = 'ahly jooa gfix cojy'
+EMAIL_HOST_USER = #the email
+EMAIL_HOST_PASSWORD = #app password
